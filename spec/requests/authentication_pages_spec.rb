6eabf2095233a295c910232bed9cb57aea736fe2
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-01-18 00:37:49 -0700

    make sure signed out test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Authentication" do
     it { should have_selector('title', text: 'Sign in') }
   end
 
-    describe "signin" do
+describe "signin" do
     before { visit signin_path }
     
 
@@ -40,6 +40,12 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+   
+
+        describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
 end
 end
