92242f7df7e22f547a834e7c72378057abd80742
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-01-26 15:48:58 -0700

    move end to correct place

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -5,6 +5,8 @@ def full_title(page_title)
   else
     "#{base_title} | #{page_title}"
   end
+end
+
 def sign_in(user)
   visit signin_path
   fill_in "Email",    with: user.email
@@ -12,5 +14,4 @@ def sign_in(user)
   click_button "Sign in"
   # Sign in when not using Capybara as well.
   cookies[:remember_token] = user.remember_token
-end
 end
